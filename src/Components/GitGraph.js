909b3533fcd9100a84599d8f79afda7444cdaad0
Yash Bhawsar <yashbhawsar97@gmail.com>
2024-11-13 23:15:47 +0530

    no

--- src/Components/GitGraph.js
+++ src/Components/GitGraph.js
@@ -1,7 +1,52 @@
 import React from "react";
 
 const GitGraph = () => {
-  return <div>GitGraph</div>;
+  return (
+    <>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna
+        nisi, vulputate non leo vel, suscipit accumsan ante. Nullam ut lacus
+        finibus, commodo orci at, imperdiet tortor. Pellentesque at orci ac dui
+        rhoncus euismod. Aenean ultricies, diam a elementum vulputate, tortor
+        enim sollicitudin tellus, et facilisis tortor mauris sed velit. Cras nec
+        vestibulum augue. Proin et erat eu enim elementum posuere. Praesent eget
+        elit id erat commodo hendrerit suscipit vitae massa. Praesent sagittis
+        lobortis eros ut luctus. Vivamus interdum malesuada libero, ac efficitur
+        nisl commodo eget. Sed in ultrices quam, sed pretium arcu. Vestibulum
+        aliquam pellentesque enim aliquet pulvinar. Phasellus quis erat nisi.
+        Morbi condimentum nibh vel massa cursus, non consectetur metus
+        ultricies. Fusce et urna tempor, fermentum augue pulvinar, accumsan mi.
+        Suspendisse fringilla purus mauris, fringilla tempor justo faucibus a.
+        Phasellus eget massa vitae ex fringilla dictum vitae et ante. Proin quam
+        diam, mollis et ipsum sit amet, egestas viverra nulla. Aenean hendrerit
+        velit orci. Maecenas ac justo quis est lobortis porta. Sed a libero at
+        est sagittis ornare. Mauris scelerisque nulla nec ipsum viverra semper.
+        Aliquam lorem ipsum, commodo gravida dolor eget, lacinia fringilla
+        augue. Vestibulum dapibus in dolor sit amet sollicitudin. In risus odio,
+        egestas varius placerat in, sollicitudin at tortor. Duis pharetra ex
+        lacus, vel rhoncus eros finibus eu. Ut laoreet leo vitae blandit
+        ultrices. Praesent feugiat turpis at congue finibus. Cras placerat et
+        felis convallis fringilla. Suspendisse mattis, enim ut ornare mattis,
+        eros tellus tempus erat, eget gravida justo felis a felis. Donec et urna
+        ac tortor ullamcorper blandit at in eros. Curabitur id lacus efficitur
+        risus vulputate egestas. Nulla facilisi. Fusce congue dui vitae eros
+        fermentum, id ullamcorper felis tincidunt. Nunc eu vulputate nisl.
+        Integer id cursus neque. Orci varius natoque penatibus et magnis dis
+        parturient montes, nascetur ridiculus mus. Vivamus imperdiet a risus
+        vitae tristique. Quisque commodo metus aliquet, lobortis tellus vitae,
+        bibendum erat. Etiam eu auctor purus. Duis volutpat, tellus aliquet
+        commodo viverra, eros nibh bibendum lorem, vitae imperdiet dolor orci ut
+        purus. Maecenas ultricies facilisis dolor, sed pharetra lorem. Morbi
+        vitae mattis magna, quis scelerisque mauris. Curabitur non maximus
+        neque, vel condimentum eros. Duis et malesuada libero. Morbi iaculis ut
+        urna sit amet ultricies. Maecenas nec rhoncus arcu, at vulputate tellus.
+        Aliquam aliquam arcu nibh, eget cursus purus molestie a. In sodales
+        posuere magna, eu mollis neque finibus consequat. Mauris vel euismod
+        lacus, eget vestibulum nisl. Suspendisse eget vestibulum justo, sodales
+        eleifend felis. Fusce lobortis metus vel tempor efficitur.
+      </p>
+    </>
+  );
 };
 
 export default GitGraph;
